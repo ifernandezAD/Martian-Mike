2d83cb13c40b77cf939fc1d2bd6b6ce5dd60cf16
IFernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-05-22 12:32:56 +0200

    Level timer

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
+@export var level_time = 5
 
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var deathzone = $Deathzone
 
 var player = null
+var timer_node = null
+var time_left
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -21,6 +25,23 @@ func _ready():
 	
 	exit.body_entered.connect(_on_exit_body_entered)
 	deathzone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	
+	timer_node = Timer.new()
+	timer_node.name="Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -=1
+		print(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -43,5 +64,6 @@ func _on_exit_body_entered(body):
 		if next_level !=null:
 			exit.animate()
 			player.active = false
+			win = true
 			await get_tree().create_timer(1.5).timeout
 			get_tree().change_scene_to_packed(next_level)
